d6b6c2dae57b0bd4e10c13ad0a5396c10717e9ec
Bert van der Weerd <bert@superstring.nl>
2009-10-13 01:13:00 +0200

    the win32 config.h file was being ignored while it should not have been

---
+++ win32/src/config.h
@@ -0,0 +1,11 @@
+#define PACKAGE_NAME       "sprot"
+#define PACKAGE_VERSION    "0.1.15"
+#define PACKAGE_BUGREPORT  "bert@superstring.nl"
+
+#pragma warning (disable:4786)
+
+#if !defined (INVALID_FILE_ATTRIBUTES)
+# define INVALID_FILE_ATTRIBUTES ((DWORD)-1)
+#endif
+
+//#define _WIN32_IE 0x0500
